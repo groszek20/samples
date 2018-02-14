5bc875f5e80e7db557822441004957206f1562d4
groszek20 <pea@op.pl>
2018-02-14 22:23:11 +0100

    test NG

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/61/30f759130211001818eba98144465153
@@ -0,0 +1,8 @@
+package sample;
+
+public class RandomEmailGenerator {
+
+	public String generate() {
+		return "adam@email.pl";
+	}
+}
--- sample/src/main/java/sample/mainApp.java
+++ sample/src/main/java/sample/mainApp.java
@@ -1,5 +1,60 @@
 package sample;
 
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
 public class mainApp {
 
+	public RandomEmailGenerator obj = new RandomEmailGenerator();
+	public String email = obj.generate();
+	
+	@Test
+	public void testEmail() {
+		System.out.println("testEmail");
+		Assert.assertEquals(email, "adam@email.pl");
+	}
+	
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("Before Suite");
+	}
+	
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("After Suite");
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Before Test");
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		System.out.println("After Test");
+	}
+	
+	@BeforeGroups
+	public void beforeGroups() {
+		System.out.println("Before Groups");
+	}
+	
+	@AfterGroups
+	public void afterGroups() {
+		System.out.println("After Groups");
+	}
+	
+	@Test
+	public void runTest2() {
+		System.out.println("runTest2");
+		Assert.assertNotNull(email);
+	}
+	
 }
