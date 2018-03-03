d95f4ec4c9384c2aa1c3d82228290e59427d3a5c
groszek20 <pea@op.pl>
2018-03-03 18:32:12 +0100

    selenium

--- sample/src/main/java/seleniumTutorial/MailLogin.java
+++ sample/src/main/java/seleniumTutorial/MailLogin.java
@@ -1,30 +1,49 @@
 package seleniumTutorial;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class MailLogin {
 
 	private WebDriver driver ;
 	private String url;
 	private String login;
+	private String passWord;
 	
 	
 	@Before
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Adam\\Downloads\\chromedriver.exe");
 		driver = new ChromeDriver();
-		url = "https://www.youtube.com/";
+		url = "https://www.onet.pl/";
 		login = "pea";
+		passWord = "dupa";
 	}
 	
 	@Test
 	public void loginNegative() {
 		driver.get(url);
+		driver.findElement(By.partialLinkText("E-MAIL")).click();
+		
+		WebElement loginElement = driver.findElement(By.name("login"));
+		loginElement.clear();
+		loginElement.sendKeys(login);
+		
+		WebElement passwordElement = driver.findElement(By.id("f_password"));
+		passwordElement.clear();
+		passwordElement.sendKeys(passWord);
+		
+		driver.findElement(By.xpath("//input[@value='Zaloguj']")).click();
+		
+		Assert.assertFalse(driver.getPageSource().contains("Wprowadü poprawny adres e-mail"));
 	}
 	
 	@After
